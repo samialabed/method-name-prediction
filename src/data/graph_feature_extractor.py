829105a4432adca02f65348fb8199b27c5eeaeb3
samialab <6316945+samialab@users.noreply.github.com>
2019-02-11 22:51:33 +0000

    Add function to separate method's name and body

--- src/data/graph_feature_extractor.py
+++ src/data/graph_feature_extractor.py
@@ -6,30 +6,45 @@ from data.graph_pb2 import FeatureNode, Graph
 
 # TODO add a method to separate signature from body
 class GraphFeatureExtractor(object):
-    def __init__(self, graph: Graph, remove_short_methods: bool, remove_override_methods: bool):
+    def __init__(self, graph: Graph,
+                 remove_override_methods: bool,
+                 min_line_of_codes: int = 3):
+        """
+        Extract features from graph_pb2.py graph.
+
+        :param graph: a graph_pb2.py graph.
+        :param remove_override_methods: remove methods with override.
+        :param min_line_of_codes: minimum line of codes each method should contain. including the method signature.
+        """
         self.graph = graph
         self.edges_map = self.edge_list_to_map()
         self.tokens_to_content_map = self.map_tokens_id_to_content()
-        self.remove_empty_methods = remove_short_methods
         self.remove_override_methods = remove_override_methods
+        self.min_line_of_codes = min_line_of_codes
 
-    def retrieve_methods_content(self) -> List[List[str]]:
-        """ Retrieve the content of every method including signature and body"""
-        tokens_list = []
+    def retrieve_methods_content(self) -> List[(str, List[str])]:
+        """
+        Retrieve the content of every method separting the signature and body
+        :return list of tuple (method, list of each token of the method's body)
+
+        Example return: [(method1, [method1_body_tokens]), (method2, [method2_body_tokens]),...]
+        """
+        methods_name_body_list = []
 
         method_nodes = self.find_all_method_nodes()
         for method_node in method_nodes:
+            if method_node.endLineNumber - method_node.startLineNumber < self.min_line_of_codes:
+                continue
+
             method_token_list = self.extract_body_and_signature(method_node)
             if self.remove_override_methods:
                 # don't add tokens that have override in them
                 if 'monkeys_at' == method_token_list[0] and 'override' == method_token_list[1]:
                     continue
-            if self.remove_empty_methods and len(method_token_list) < 10:
-                # don't add abstracts, short and no body methods.
-                continue
 
-            tokens_list.append(method_token_list)
-        return tokens_list
+            name, body = self.separate_method_name_from_body(method_token_list)
+            methods_name_body_list.append((name, body))
+        return methods_name_body_list
 
     def find_all_method_nodes(self) -> List[FeatureNode]:
         """ Return list of all nodes that are method nodes. """
@@ -70,3 +85,23 @@ class GraphFeatureExtractor(object):
                 out.append((token_content, child_id))
             else:
                 self._dfs(child_id, out)
+
+    @staticmethod
+    def separate_method_name_from_body(method_token: List[str]) -> (str, List[str]):
+        method_name = ''
+        for idx, token in enumerate(method_token):
+            # the method name is the first token that comes before '('
+            if idx + 1 < len(method_token) and method_token[idx + 1] == 'lparen':
+                method_name = token
+
+            if token == 'lbrace':
+                # the body is everything after open brace '{' up to the very end which is '}'
+                body = method_token[idx + 1: len(method_token) - 1]
+
+                assert method_name != '', 'Method name should not be empty'
+                assert len(body) != 0, 'Method body should not be empty'
+
+                return method_name, body
+
+        raise Exception(
+            'Failed to separate the method name and body from the token.')  # Should I include the token used?
