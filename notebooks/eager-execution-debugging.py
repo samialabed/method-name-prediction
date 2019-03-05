5e0b649b2340dbc971e60f4fe0c242792d82462b
samialab <6316945+samialab@users.noreply.github.com>
2019-03-05 12:11:18 +0000

    Update experiements

--- notebooks/eager-execution-debugging.py
+++ notebooks/eager-execution-debugging.py
@@ -3,7 +3,7 @@ from tensorflow.python import keras
 from tensorflow.python.keras import layers
 
 from data.preprocess import PreProcessor
-from data.utils import translate_tokenized_array_to_list_words
+from data.utils import translate_tokenized_array_to_list_words, predict_name
 from models.cnn_attention import ConvAttention
 from utils.activations import masked_sparse_cross_entropy_loss
 
@@ -12,13 +12,13 @@ tf.enable_eager_execution()
 # data = PreProcessor(config=PreProcessor.DEFAULT_CONFIG,
 #                     data_dir='data/raw/r252-corpus-features/org/elasticsearch/action/admin/')
 data = PreProcessor(config=PreProcessor.DEFAULT_CONFIG,
-                    data_dir='../data/raw/r252-corpus-features/org/elasticsearch/action/admin/cluster/allocation/')
+                    data_dir='data/raw/r252-corpus-features/org/elasticsearch/action/admin/')
 
 vocab = data.metadata['token_vocab']
 processed = data.get_tensorise_data()
 
 embedding_dim = 128
-vocabulary_size = len(vocab) + 2
+vocabulary_size = len(vocab) + 1
 max_chunk_length = data.config['max_chunk_length']
 code_snippet = processed['body_tokens']
 # code_snippet = np.expand_dims(processed['body_tokens'], 0)
@@ -43,16 +43,9 @@ dropout_rate = 0.5
 main_input = layers.Input(shape=(max_chunk_length,),
                           batch_size=batch_size,
                           dtype=tf.int32, name='main_input')
-
-cnn_layer = ConvAttention(vocabulary_size=vocabulary_size,
-                          embedding_dim=embedding_dim,
-                          max_chunk_length=max_chunk_length,
-                          k1=k1,
-                          k2=k2,
-                          w1=w1,
-                          w2=w2,
-                          w3=w3,
-                          dropout_rate=dropout_rate)
+hyperparameter = {'batch_size': 1, 'k1': 8, 'k2': 8, 'w1': 24, 'w2': 29, 'w3': 10, 'dropout_rate': 0.5,
+                  'max_chunk_length': max_chunk_length, 'vocabulary_size': vocabulary_size, 'embedding_dim': 128}
+cnn_layer = ConvAttention(hyperparameter)
 
 optimizer = keras.optimizers.Nadam()  # RMSprop with Nesterov momentum
 loss_func = masked_sparse_cross_entropy_loss
@@ -75,17 +68,16 @@ model.compile(optimizer=optimizer,
 #                     callbacks=[tbCallBack],
 #                     validation_split=0.2)
 
+model.load_weights('model.h5')
+
 code_snippet = processed['body_tokens']
 label_name = processed['name_tokens']
 dataset = tf.data.Dataset.from_tensor_slices((code_snippet, label_name))
 dataset = dataset.shuffle(1000).batch(1)
 
 for (batch, (cd_block, meth_name)) in enumerate(dataset.take(1)):
-    with tf.GradientTape() as tape:
-        logits = model(cd_block, training=True)
-        loss_value = tf.losses.sparse_softmax_cross_entropy(meth_name, logits)
-# translate prediction
+    test = predict_name(vocab, model, cd_block.numpy())
+    # print(model.predict(cd_block.numpy()))
+    print(test)
 
-prediction = model.predict(code_snippet[21].reshape(1, -1))
-print(prediction)
-print(translate_tokenized_array_to_list_words(vocab, prediction.argmax(2)[0]))
+# translate prediction
