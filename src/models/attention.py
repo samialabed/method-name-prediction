bff676857bf62f69f994037f586966fe625371d6
samialab <6316945+samialab@users.noreply.github.com>
2019-03-06 10:51:40 +0000

    Use loggers instead of print

--- src/models/attention.py
+++ src/models/attention.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 
 from tensorflow import Tensor
@@ -5,41 +6,21 @@ from tensorflow.python.keras import backend, layers, models
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.layers import Lambda, Dropout, Conv1D, Softmax, TimeDistributed
 
-"""
-Attention features and weight as defined in [1] (page 3)
 
-
-[1] Allamanis, M., Peng, H. and Sutton, C., 2016, June. 
-A convolutional attention network for extreme summarization of source code. 
-In International Conference on Machine Learning (pp. 2091-2100).
-https://arxiv.org/abs/1602.03001
-"""
-
-
-# TODO add loggers instead of print
 class AttentionFeatures(models.Model):
     """
-        <From the paper>
-        attention_features is that given the input c, it uses convolution to compute k2 features for each location.
-        By then using ht−1 as a multiplicative gating-like mechanism, only the
-        currently relevant features are kept in L2. In the final stage,
-        we normalize L2. attention_features is described with the
-        following pseudocode:
-        attention_features (code tokens c, context ht−1)
-        C ← LOOKUPANDPAD(c, E)
-        L1 ← RELU(CONV1D(C, Kl1))
-        L2 ← CONV1D(L1, Kl2) * ht−1
-        L_feat ← L2/ kL2k2
-        return L_feat
+        <From the paper page 3>
+        Attention_features is that given the input c, it uses convolution to compute k2 features for each location.
+        By then using ht−1 as a multiplicative gating-like mechanism.
+        Only the currently relevant features are kept in L2. In the final stage,
+
+        :arg w1, w2: the window sizes of the convolution
+        :arg k1: number of filters on top of the embedding of size w1.
     """
 
     def __init__(self, k1: int, w1: int, k2: int, w2: int, dropout_rate: float):
-        # embedding_dim = D in the paper
-        # w1, w2 are the window sizes of the convolutions, hyperparameters
-        # ht−1 ∈ R represents information from the previous subtokens m0 . . . mt−1
         super().__init__()
-        # Use 1D convolutions as input is text.
-        # create k1 filters, each of window size of w1, the output is k1 different convolutions.
+        self.logger = logging.getLogger(__name__)
         # causal padding to ensure the conv keep the size of the input throughout
         # Keras requires the input to be the same size as the output
         self.conv1 = TimeDistributed(Conv1D(k1, w1, activation='relu', padding='causal', name='attention_fet_conv1'))
@@ -49,25 +30,25 @@ class AttentionFeatures(models.Model):
 
     def call(self, inputs: List[Tensor], training=False, **kwargs):
         C, h_t = inputs  # C is code_tokens, h_t is the previous hidden state
-        print("AttentionFeatures: C shape = {}, h_t shape = {}".format(C.shape, h_t.shape))
+        self.logger.info("C shape = {}, h_t shape = {}".format(C.shape, h_t.shape))
         # C = [batch size, max chunk size, emb dim]
-        # h_t = [batch size, k2]
+        # h_t = [batch size, k2], represents information from the previous subtokens m0 . . . mt−1
 
         L_1 = self.conv1(C)
-        print("AttentionFeatures: L_1 shape = {}".format(L_1.shape))
+        self.logger.info("L_1 shape = {}".format(L_1.shape))
         # L_1 = [batch size, max chunk size, k1]
         L_1 = self.dropout(L_1, training=training)
         L_2 = self.conv2(L_1)
-        print("AttentionFeatures: L_2 shape = {}".format(L_2.shape))
+        self.logger.info("L_2 shape = {}".format(L_2.shape))
         # elementwise multiplication with h_t to keep only relevant features (acting like a gating-like mechanism)
         L_2 = layers.Multiply(name='attention_fet_l2_mul')([L_2, h_t])
 
         # L_2 = [batch size, max chunk size, k2]
-        print("AttentionFeatures: L_2 shape  after multiply = {}".format(L_2.shape))
+        self.logger.info("L_2 shape  after multiply = {}".format(L_2.shape))
         L_2 = self.dropout(L_2, training=training)
         # perform L2 normalisation
         L_feat = self.l2_norm(L_2)
-        print("AttentionFeatures: L_feat shape = {}".format(L_feat.shape))
+        self.logger.info("L_feat shape = {}".format(L_feat.shape))
         return L_feat
 
 
@@ -82,17 +63,17 @@ class AttentionWeights(models.Model):
     def __init__(self, w3, dropout_rate):
         # w3 are the window sizes of the convolutions, hyperparameters
         super().__init__()
-        self.conv1 = TimeDistributed(
-            Conv1D(1, w3, activation=None, padding='causal', use_bias=True, name='atn_weight_conv1'))
+        self.logger = logging.getLogger(__name__)
+        self.conv1 = TimeDistributed(Conv1D(1, w3, activation=None, padding='causal', name='atn_weight_conv1'))
         self.dropout = Dropout(dropout_rate)
         self.softmax = Softmax(name='atn_weight_softmax')
 
     def call(self, l_feat_and_input_mask: List[Tensor], training=False, **kwargs):
         l_feat, mask = l_feat_and_input_mask
-        print("AttentionWeights: l_feat shape = {}".format(l_feat.shape))
+        self.logger.info("L_feat shape = {}".format(l_feat.shape))
 
         attention_weight = self.conv1(l_feat)
-        print("AttentionWeights: attention_weight shape = {}".format(attention_weight.shape))
+        self.logger.info("attention_weight shape = {}".format(attention_weight.shape))
         # attention_weight = [batch size, max chunk size, 1]
         attention_weight = self.dropout(attention_weight, training=training)
         # Give less weights to masked value
--- src/models/cnn_attention.py
+++ src/models/cnn_attention.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 
 from tensorflow.python import keras, Tensor
@@ -20,6 +21,7 @@ class ConvAttention(keras.Model):
 
     def __init__(self, hyperparameters: Dict[str, any]):
         super().__init__()
+        self.logger = logging.getLogger(__name__)
         vocabulary_size = hyperparameters['vocabulary_size']
         embedding_dim = hyperparameters['embedding_dim']
         max_chunk_length = hyperparameters['max_chunk_length']
@@ -47,7 +49,6 @@ class ConvAttention(keras.Model):
                                     initializer='zeros',
                                     trainable=True)
         self.softmax_layer = TimeDistributed(Softmax())
-        # self.dense_layer = TimeDistributed(Dense(vocabulary_size, activation='softmax', name='cnn_att_dense'))
 
     def call(self, code_block: Tensor, training=False, **kwargs):
         # Note: all layers are wrapped with TimeDistributed, thus the shapes have number of
@@ -57,43 +58,43 @@ class ConvAttention(keras.Model):
         # create a mask of the padding sequence of the input
         mask_vector = K.cast(K.equal(code_block, 0), dtype='float32') * -1e7
         # mask_vector [batch size, max chunk length, 1]
-        print("ConvAttention: mask_vector shape = {}".format(mask_vector.shape))
+        self.logger.info("ConvAttention: mask_vector shape = {}".format(mask_vector.shape))
 
         # code_block = Masking(mask_value=0, )(code_block)
         tokens_embedding = self.embedding_layer(code_block)
-        print("ConvAttention: Tokens shape = {}".format(tokens_embedding.shape))
+        self.logger.info("ConvAttention: Tokens shape = {}".format(tokens_embedding.shape))
         # tokens_embedding = [batch_size, max chunk length, embedding_dim]
 
         _, h_t = self.gru_layer(tokens_embedding, training=training)
         # h_t = [batch_size, k2)
-        print("ConvAttention: h_t shape = {}".format(h_t.shape))
+        self.logger.info("ConvAttention: h_t shape = {}".format(h_t.shape))
         l_feat = self.attention_feature_layer([tokens_embedding, h_t])
-        print("ConvAttention: L_feat shape = {}".format(l_feat.shape))
+        self.logger.info("ConvAttention: L_feat shape = {}".format(l_feat.shape))
 
         # L_feat = [batch size, max chunk size, k2]
         alpha = self.attention_weights_layer([l_feat, mask_vector])
-        print("ConvAttention: alpha shape = {}".format(alpha.shape))
+        self.logger.info("ConvAttention: alpha shape = {}".format(alpha.shape))
         # alpha = [batch size, max chunk size] weights over embeddings
 
         # apply the attention to the input embedding
         n_hat = K.sum((K.expand_dims(alpha, axis=-1) * tokens_embedding), axis=1)
-        print("ConvAttention: n_hat shape = {}".format(n_hat.shape))
+        self.logger.info("ConvAttention: n_hat shape = {}".format(n_hat.shape))
         # n_hat = [batch size, embedding dim]
 
         # embedding over all vocabulary
         E = self.embedding_layer.layer.embeddings
-        print("ConvAttention: E shape = {}".format(E.shape))
+        self.logger.info("ConvAttention: E shape = {}".format(E.shape))
         # E = [vocabulary size, embedding dim]
 
         # Apply attention to the words over all embeddings
         n_hat_E = K.nn.math_ops.tensordot(E, K.transpose(n_hat), axes=[[1], [0]])
         # n_hat_E = [vocabulary size, max chunk size, batch size]
         n_hat_E = K.permute_dimensions(n_hat_E, [2, 1, 0])
-        print("ConvAttention: n_hat_E shape = {}".format(n_hat_E.shape))
+        self.logger.info("ConvAttention: n_hat_E shape = {}".format(n_hat_E.shape))
         # n_hat_E = [batch size, max chunk size, vocabulary size]
 
         n = self.softmax_layer(K.bias_add(n_hat_E, self.bias))
-        print("ConvAttention: n shape = {}".format(n.shape))
+        self.logger.info("ConvAttention: n shape = {}".format(n.shape))
         # n = [batch size, vocabulary size] the probability of each token in the vocabulary
 
         return n
